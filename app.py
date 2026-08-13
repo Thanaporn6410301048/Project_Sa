8f88ff3797b08950eada21a858199a1904fef9a0
Thanaporn6410301048 <6410301048@cdti.ac.th>
2026-08-13 13:49:49 +0700

    10000

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Ripple Coffee Roasters สาขา 100 "
+    return "Ripple Coffee Roasters สาขา 100000 "
 
 if __name__ == "__main__":
     app.run(debug=True)
