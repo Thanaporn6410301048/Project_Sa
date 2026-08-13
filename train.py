0bcd4dd4598ff191d706eb47c61518274005836c
Thanaporn6410301048 <6410301048@cdti.ac.th>
2026-08-13 14:03:25 +0700

    test

---
+++ train.py
@@ -0,0 +1,17 @@
+import joblib
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+# 1. เตรียมข้อมูลสมมติ (ขนาดบ้าน ตารางเมตร กับ ราคา ล้านบาท)
+# บ้านขนาด 30, 50, 80, 100 ตารางเมตร
+X = np.array([[30], [50], [80], [100]])
+# ราคา 1.5, 2.5, 4.0, 5.0 ล้านบาท (ตามลำดับ)
+y = np.array([1.5, 2.5, 4.0, 5.0])
+
+# 2. สร้างโมเดลและสั่งให้เรียนรู้ (Train)
+model = LinearRegression()
+model.fit(X, y)
+
+# 3. เซฟสมองก้อนนี้เก็บไว้ในไฟล์ชื่อ house_model.pkl
+joblib.dump(model, "house_model.pkl")
+print("เทรนโมเดลเสร็จแล้ว และเซฟไฟล์เรียบร้อย!")
